6e809b4bd5e353a406ffd29964105c10440c7043
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2022-05-31 15:52:03 -0500

    added react persist

--- redux/ConfigureStore.js
+++ redux/ConfigureStore.js
@@ -1,8 +1,17 @@
 import { createStore } from 'redux';
 import { spices } from './spices';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+	key: 'root',
+	storage,
+	debug: true
+};
 
 export const ConfigureStore = () => {
-	const store = createStore(spices);
+	const store = createStore(persistReducer(config, spices));
+	const persistor = persistStore(store);
 
-	return store;
+	return { persistor, store };
 };
